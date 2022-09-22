9c139bba59e50f9c11b492d3b588be02e3ce9a49
hamza <hamzajjmalik@gmail.com>
2022-09-23 01:31:45 +0500

    initial commit

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,27 +4,7 @@ const bcrypt = require("bcrypt");
 const Joi = require("joi")
 
 router.post("/api/auth", async (req, res) => {
-	/*try {
-		const  error  = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message })
-
-		const user = await User.findOne({ email: req.body.email });
-		if (!user)
-			return res.status(401).send({ message: "Invalid Email or Password" })
-
-		const validPassword = await bcrypt.compare(
-			req.body.password,
-			user.password
-		)
-		if (!validPassword)
-			return res.status(401).send({ message: "Invalid Email or Password" })
-
-		const token = user.generateAuthToken();
-		res.status(200).send({ data: token, message: "logged in successfully" }) 
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	} */ var {email,password}=req.body
+	 var {email,password}=req.body
     if(!email || !password )
     {
         return res.status(422).json({error:"Please add all fields"})
